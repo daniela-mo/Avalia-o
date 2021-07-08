f6c6cb753131c7d6552a839cbd792925e8126d79
Daniela-MMO <danni.oliver94@gmail.com>
2021-07-08 11:54:07 -0300

    avaliacao trainee

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<style lang="scss">
+*,
+*::before,
+*::after {
+  margin: 0;
+  padding: 0;
+  font-family: Poppins, sans-serif;
+  box-sizing: border-box;
+}
+
+html,
+body,
+#app {
+  background: #e5e5e5;
+}
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="modal-all">
+    <div class="modal-all__content">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.modal-all {
+  background: rgb(15, 15, 15, 0.15);
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  &__content {
+    width: 544px;
+    height: 539px;
+    background: #fff;
+    border-top-left-radius: 4px;
+    border-top-right-radius: 4px;
+  }
+}
+</style>
